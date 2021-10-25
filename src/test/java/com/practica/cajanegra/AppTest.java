ff50ec562aa581b6b8d8fc3bbd7785efa7643ebc
luiscal4a <luiscal4a@gmail.com>
2021-10-25 19:38:47 +0200

    Created test file and added singlelist dependency

---
+++ src/test/java/com/practica/cajanegra/AppTest.java
@@ -0,0 +1,23 @@
+package com.practica.cajanegra;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+
+public class AppTest {
+	
+  
+}
+  
